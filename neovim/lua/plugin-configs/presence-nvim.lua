d4ac503c0273a622ccdf8be88dff350ed9a0f486
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-06-01 22:54:14 -0700

    Adjusting presence-nvim config

--- neovim/lua/plugin-configs/presence-nvim.lua
+++ neovim/lua/plugin-configs/presence-nvim.lua
@@ -1,19 +1,19 @@
-local g = vim.g
+require('presence'):setup({
+	-- Presence will update as file changes
+	auto_update = true,
 
---Presence will update as file changes
-g.presence_auto_update = true
+	-- Text that appears when you hover over NVim logo
+	neovim_image_text = 'neovim.io',
 
---Text that appears when you hover over NVim logo
-g.presence_neovim_image_text = 'neovim.io'
+	-- Main image is the file type
+	main_image = 'file',
 
---Main image is the file type
-g.presence_main_image = 'file'
+	-- Don't show the line I'm on
+	enable_line_number = false,
 
--- Don't show the line I'm on
-g.presence_enable_line_number = false
+	-- Blacklisted files from Discord status
+	blacklist = {},
 
--- Blacklisted files from Discord status
-g.presence_blacklist = {}
-
--- Buttons never even worked
-g.presence_buttons = false
+	-- Buttons never even worked
+	buttons = false,
+})
