bde443323f6078da0416f43b557c3b1573f82cc4
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-06-22 10:09:04 -0700

    Adding more LSP servers and labelling them

--- neovim/lua/plugin-configs/lsp.lua
+++ neovim/lua/plugin-configs/lsp.lua
@@ -1,16 +1,23 @@
 -- LSP servers that I want to have installed
-local servers = { "sumneko_lua", "tsserver", "jsonls", "bashls" }
+local servers = {
+	"sumneko_lua", -- Lua
+	"tsserver", -- TypeScript/JavaScript
+	"jsonls", -- JSON
+	"bashls", -- Bash/Zsh
+	"gopls", -- Go
+	"rust-analyzer", -- Rust
+}
 
 require('nvim-lsp-installer').setup({
-	ensure_installed = servers,
-	ui = {
-		icons = {
-			server_installed = "✓",
-			server_pending = "➜",
-			server_uninstalled = "✗"
+		ensure_installed = servers,
+		ui = {
+			icons = {
+				server_installed = "✓",
+				server_pending = "➜",
+				server_uninstalled = "✗"
+			}
 		}
-	}
-})
+	})
 
 vim.g.coq_settings = {
 	auto_start = 'shut-up'
