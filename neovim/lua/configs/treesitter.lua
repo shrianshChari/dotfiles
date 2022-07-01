3274423ad298afb78017bf15bd60c736fb3958a5
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-07-01 01:08:12 -0700

    Don't need to install all treesitter stuff at once

--- neovim/lua/configs/treesitter.lua
+++ neovim/lua/configs/treesitter.lua
@@ -1,7 +1,35 @@
 local configs = require("nvim-treesitter.configs")
 
+local langs = {
+	'typescript',
+	'javascript',
+	'java',
+	'json',
+	'json5',
+	'jsonc',
+	'hjson',
+	'html',
+	'scss',
+	'css',
+	'vim',
+	'yaml',
+	'c',
+	'bash',
+	'go',
+	'rust',
+	'latex',
+	'lua',
+	'python',
+	'toml',
+	'tsx',
+	'toml',
+	'markdown',
+	'markdown_inline',
+	'make',
+}
+
 configs.setup {
-	ensure_installed = "all",
+	ensure_installed = langs,
 	highlight = {
 		enable = true,
 		aditional_vim_regex_highlighting = false,
