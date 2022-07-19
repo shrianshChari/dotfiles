0db99d9e380c11e48d8e51fcf9c5783e7836ebf9
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-07-19 00:47:38 -0700

    Modifying NvimTree configs

--- neovim/lua/configs/nvimtree.lua
+++ neovim/lua/configs/nvimtree.lua
@@ -9,13 +9,29 @@ nmap('<C-n>', ':NvimTreeToggle<CR>', {}) -- Toggle NvimTree
 require('nvim-tree').setup {
 	hijack_cursor = true,
 	create_in_closed_folder = true,
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
 	view = {
 		mappings = {
 			list = {
 				{ key = { "<C-]>", "+" }, action = "cd", mode = "n" },
 				{ key = { "<C-v>", "v" }, action = "vsplit", mode = "n" },
-				{ key = { "<C-x>", "h" }, action = "split", mode = "n" }
+				{ key = { "<C-x>", "h" }, action = "split", mode = "n" },
 			},
 		},
 	},
+	renderer = {
+		add_trailing = true,
+		icons = {
+			git_placement = "after",
+		}
+	},
+	filters = {
+		dotfiles = true, -- You can still press "H" to toggle dotfiles
+	}
 }
