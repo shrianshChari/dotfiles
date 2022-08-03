3f09df2739d7c4635da1bbe0a69887b64a3656b9
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-08-02 19:37:49 -0700

    Created and added tmux config

---
+++ scripts/tmux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MANAGER_INSTALL="apt install"
+DEB_INSTALL="dpkg -i"
+
+sudo ${MANAGER_INSTALL} tmux -y
+
+if [ -f "$HOME/.tmux.conf" ]; then
+	rm $HOME/.tmux.conf
+fi
+ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+
+echo "Tmux symlinks created!"
+# echo "Be sure to run \"vim -c 'PlugInstall'\" when first opening Vim!"
