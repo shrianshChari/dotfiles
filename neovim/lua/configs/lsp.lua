9dbbbeeafba2f107be20f295f65e675be37989b7
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-08-14 12:03:52 -0700

    Refactoring lsp-zero config, new icons

--- neovim/lua/configs/lsp.lua
+++ neovim/lua/configs/lsp.lua
@@ -13,14 +13,28 @@ local servers = {
 require('nvim-lsp-installer').setup({
 	ui = {
 		icons = {
-			server_installed = "✓",
-			server_pending = "➜",
-			server_uninstalled = "✗"
+			server_installed = "",
+			server_pending = "",
+			server_uninstalled = ""
 		}
 	}
 })
 
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-lsp.ensure_installed(servers)
-lsp.setup()
+local lspzero = require('lsp-zero')
+lspzero.set_preferences({
+	suggest_lsp_servers = true,
+  setup_servers_on_start = true,
+  set_lsp_keymaps = true,
+  configure_diagnostics = true,
+  cmp_capabilities = true,
+  manage_nvim_cmp = true,
+  call_servers = 'local',
+  sign_icons = {
+    error = '',
+    warn = '卑',
+    hint = '',
+    info = ''
+  }
+})
+lspzero.ensure_installed(servers)
+lspzero.setup()
