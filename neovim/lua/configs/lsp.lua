b0c31fcde9785c7ce43def52f5a3bf135a35ecef
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-08-15 01:06:18 -0700

    Reindenting configurations

--- neovim/lua/configs/lsp.lua
+++ neovim/lua/configs/lsp.lua
@@ -23,18 +23,18 @@ require('nvim-lsp-installer').setup({
 local lspzero = require('lsp-zero')
 lspzero.set_preferences({
 	suggest_lsp_servers = true,
-  setup_servers_on_start = true,
-  set_lsp_keymaps = true,
-  configure_diagnostics = true,
-  cmp_capabilities = true,
-  manage_nvim_cmp = true,
-  call_servers = 'local',
-  sign_icons = {
-    error = '',
-    warn = '卑',
-    hint = '',
-    info = ''
-  }
+	setup_servers_on_start = true,
+	set_lsp_keymaps = true,
+	configure_diagnostics = true,
+	cmp_capabilities = true,
+	manage_nvim_cmp = true,
+	call_servers = 'local',
+	sign_icons = {
+		error = '',
+		warn = '卑',
+		hint = '',
+		info = ''
+	}
 })
 lspzero.ensure_installed(servers)
 lspzero.setup()
