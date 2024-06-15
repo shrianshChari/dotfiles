7b413ff1cb2a59055615c7d1393c2bd8ef270818
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2024-06-15 12:00:42 -0700

    Fix custom mappings for nvimtree

--- neovim/lua/configs/nvimtree.lua
+++ neovim/lua/configs/nvimtree.lua
@@ -2,22 +2,34 @@ vim.api.nvim_set_keymap('n', 'n', ':NvimTreeFocus<CR>', { silent = true }) -- Ju
 vim.api.nvim_set_keymap('n', '<C-t>', ':NvimTreeOpen<CR>', { silent = true }) -- Enable NvimTree
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { silent = true }) -- Toggle NvimTree
 
+local function my_on_attach(bufnr)
+	local api = require('nvim-tree.api')
+
+	local function opts(descr)
+		return { desc = "nvim-tree: " .. descr, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mapings
+	vim.keymap.set('n', 'n', api.tree.open, opts('Open'))
+	vim.keymap.set('n', '<C-h>', api.tree.toggle_help, opts('Help'))
+	vim.keymap.set('n', '<C-n>', api.tree.close, opts('Close'))
+
+	vim.keymap.set('n', 'v', api.node.open.vertical, opts('Vsplit'))
+	vim.keymap.set('n', 'h', api.node.open.horizontal, opts('Hsplit'))
+
+end
+
 require('nvim-tree').setup {
+	on_attach = my_on_attach,
 	hijack_cursor = true,
 	-- create_in_closed_folder = true,
 	actions = {
 		open_file = {
 			window_picker = {
 				enable = false,
-			},
-		},
-	},
-	view = {
-		mappings = {
-			list = {
-				{ key = { "<C-]>", "+" }, action = "cd", mode = "n" },
-				{ key = { "<C-v>", "v" }, action = "vsplit", mode = "n" },
-				{ key = { "<C-x>", "h" }, action = "split", mode = "n" },
 			},
 		},
 	},
